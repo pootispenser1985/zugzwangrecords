ba4a2ee7c29f54ef27e00e9149ade69a1c7f3305
pootispenser1985 <jigostowski@gmail.com>
2017-10-31 18:52:26 -0500

    it stores the users email in db and sends them a blank email

--- inserter.php
+++ inserter.php
@@ -3,5 +3,15 @@ $email = $_POST['email'];
 $subject = "Thank you for your email address! Here's your DL code!";
 $content = "blah blah blah content";
 
+//get sql credentials. probably not how you're supposed to do it
+include '/home/thrashca/etc/mailinglist.php';
+
+//insert email address into  db. I made the field unique in mysql, is why
+//I don't check to see if it already exists or not
+$db = new mysqli('localhost', $SQLuser, $SQLpass, 'thrashca_mailinglist');
+$query = "INSERT INTO users (`email`) VALUES ('".$email."');";
+$db->query($query);
+
+//send email to user with link to downloads
 mail ($email, $subject, $content);
 ?>
