9eb5837c06a2f1479465cc7457fcd4a71ce983b3
pootispenser1985 <jigostowski@gmail.com>
2017-10-30 23:22:43 -0500

    got table set up in sql, need to write php to insert the email addresses in, check for dupes, etc

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
+  //following two functions are for showing or hiding parts of the page
   $('#contactLink').click(function() {
     $('#contact').toggle();
   });
   $('.gear-heading').click(function() {
     $(this).next().toggle();
   });
-
+  //instead of the default submit action, do submitEmail()
   $('#email-form').submit(function(event) {
     event.preventDefault();
     submitEmail();
   });
 
   function submitEmail() {
+    //get the email POST variable
     var email = $('#email').val();
-
+    /*give it to inserter.php, which will send an email to the user
+    with a randomly generated access code, and a link to the download
+    site. This is -hilariously- insecure. But, it doesn't need to be secure
+    and at this juncture, I am too dumb to do it! Shippable product. */
     $.ajax({
       type: "POST",
       url: "inserter.php",
       data: "email=" + email
     });
-  }
-});
+    //clear out the text input box
+    $('#email').val('');
+  } //end submitEmail()
+}); //end document.ready()
